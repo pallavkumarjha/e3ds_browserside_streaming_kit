31b787f35d06fba54d9f49ece8e0a3a1d4cdc788
muzaheed57 <muzaheed57@gmail.com>
2022-12-18 18:05:06 -0700

    Update integrator.js

--- integrator.js
+++ integrator.js
@@ -1,92 +1,82 @@
-function updateAppConfig() 
-	  {
-	  var obj={
-	  "useVOIP":1
-	  }
-	  
-	  return obj
-	  }
-	   
-		function afterGettingAppConfig() 
-		{
-		  console.log("ob-afterGettingAppConfig");
-		  //changeConfigValue("useVOIP",1)
-		}
-				 var appconfigPassed2Ejs ={}
-          <!-- appconfigPassed2Ejs =<%-JSON.stringify(appconfig)%> -->
-         console.log("HTML-->appconfigPassed2Ejs: " + JSON.stringify(appconfigPassed2Ejs));
-		 
-		 
-	
-		//config path
-			let isGuestMode= false
-			let app="CarConfigurator427_reimport"
-			
-			//////////////////
-			
-			let fullDomain="connector.eagle3dstreaming.com"
-			  //fullDomain="connector_staging.eaglepixelstreaming.com"
-			  //fullDomain="connector_ms6.eaglepixelstreaming.com"
-			let compnay="demo"
-			let configurationName="0AhsanConfig"
-			let MeetingId="" //"21d666ba-a0d8-4db9-9643-e74525bafe1b"
+function updateAppConfig() {
+    var obj = {
+        "useVOIP": 1
+    }
+
+    return obj
+}
+
+function afterGettingAppConfig() {
+    console.log("ob-afterGettingAppConfig");
+    //changeConfigValue("useVOIP",1)
+}
+var appconfigPassed2Ejs = {}
+
+console.log("HTML-->appconfigPassed2Ejs: " + JSON.stringify(appconfigPassed2Ejs));
 
 
-			///////////////////////////////////////////////
 
-			var part1="https://"+fullDomain 
-			var part2="/v5/"+compnay+"/"+app+"/"+configurationName
+//config path
+let isGuestMode = false
+let app = "CarConfigurator427_reimport"
 
-			////////////////////////////
+//////////////////
+
+let fullDomain = "connector.eagle3dstreaming.com"
+
+let compnay = "demo"
+let configurationName = "0AhsanConfig"
+let MeetingId = "" //"21d666ba-a0d8-4db9-9643-e74525bafe1b"
+
+
+///////////////////////////////////////////////
+
+var part1 = "https://" + fullDomain
+var part2 = "/v5/" + compnay + "/" + app + "/" + configurationName
+
+////////////////////////////
+
+
+
+
+let search = ""
+
+if ((MeetingId == "") || (MeetingId == undefined)) {
+
+} else if ((MeetingId != "") || (MeetingId != undefined)) {
+    if (!isGuestMode)
+        search = "?hostMeetingId=" + MeetingId
+    else //type="text/css" rel="stylesheet" href="assets/css/voip.css"/>
+    {
+        part2 = "/v5/" + compnay + "/" + configurationName
+
+        if ((MeetingId != "") || (MeetingId != undefined))
+            search = "?GuestMeetingId=" + MeetingId
+    }
+
+}
+
+
+
+console.log("search: " + search)
 
 			
-				
-			//window.location.search: ?GuestMeetingId=5cfa95d9-c540-455c-9086-1715c600daa1
-			let search=""
-			
-			if(  (MeetingId == "")|| (MeetingId == undefined) )
-			{
-			
-			}	
-			else if(  (MeetingId != "")|| (MeetingId != undefined) )
-			{
-				if(!isGuestMode)
-					search="?hostMeetingId="+MeetingId
-				else //type="text/css" rel="stylesheet" href="assets/css/voip.css"/>
-				{
-					part2="/v5/"+compnay+"/"+configurationName
-				
-					if( (MeetingId != "")|| (MeetingId != undefined) )
-						search="?GuestMeetingId="+MeetingId
-				}
-				
-			}
+let urlParams = new URLSearchParams(search)
+console.log("urlParams: ")
 
-			
-			
-			console.log("search: "+search)
+for (const [key, value] of urlParams) {
+    console.log(key, value);
+}
 
-//urlParams: 
-//GuestMeetingId 5cfa95d9-c540-455c-9086-1715c600daa1			
-			let urlParams = new URLSearchParams(search)	
-			console.log("urlParams: ")
 
-			for (const [key, value] of urlParams) {
-			  console.log(key, value);
-			}			
-			
-			
-			
-			let pathname = part2
-			console.log("pathname: "+pathname)
 
-			let href_emulated = 
-								part1+part2
-								//window.location.href
-								
-			href_emulated=href_emulated+search						
-			console.log("href_emulated1: "+href_emulated)
-			console.log("pathname1: "+pathname)
-			
-			//https://connector.eagle3dstreaming.com/v5/demo/CarConfigurator427/AlekseenkoConfig?hostMeetingId=dca7d0bb-aa94-418a-8fff-2eacc34ad4d8
-			//https://connector.eagle3dstreaming.com/v5/demo/CarConfigurator427/usmanDebugging?GuestMeetingId=21d666ba-a0d8-4db9-9643-e74525bafe1b
+let pathname = part2
+console.log("pathname: " + pathname)
+
+let href_emulated =part1 + part2
+
+
+href_emulated = href_emulated + search
+console.log("href_emulated1: " + href_emulated)
+console.log("pathname1: " + pathname)
+
