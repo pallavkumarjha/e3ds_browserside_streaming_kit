3d555197310383ef23a754cdcf38a09c96fb9871
Saeed Sharman <saeed@eagle3dstreaming.com>
2023-03-14 23:21:52 +0600

    adjust chanes for v1.2

--- public/pxs/scripts/preLoadScripts.js
+++ public/pxs/scripts/preLoadScripts.js
@@ -1,119 +1,37 @@
-function updateAppConfig() {
-    var obj = {
-        "useVOIP": 0
-    }
 
-    return obj
+var sendToUnreal = undefined
+var e3ds_data =
+{
+	//"domain": "connector.eagle3dstreaming.com",//"put your CP url if you have a special one "
+	"userName": "demo",//"put your user name "
+	"appName": "E3DSFeaturesTemplate", //"put your app name"
+	"configurationName": "E3DS-Iframe-Demo",//"put your config name"
+	"configuration": {
+		"useVOIP": 0,
+		"showPsControl": "1",
+		"isAdminDebugging": 1,
+	}
 }
 
-function afterGettingAppConfig() {
-    console.log("ob-afterGettingAppConfig");
-    //changeConfigValue("useVOIP",1)
-}
-var appconfigPassed2Ejs = {}
-
-console.log("HTML-->appconfigPassed2Ejs: " + JSON.stringify(appconfigPassed2Ejs));
-
-
-
-	  
-var call2EmitUIInteraction=undefined
-function afterGettingUnrealResponse(descriptor) {
-    console.log("ob-afterGettingUnrealResponse");
-    console.log("UnrealResponse: "+descriptor);
-    var event = new CustomEvent("game-event", { detail: descriptor});
-    window.dispatchEvent(event);
-	
-	//document.getElementById('webrtc-low-qp-text').value=descriptor;
-    //changeConfigValue("useVOIP",1)
+function onConfigAcquire() {
+	console.log("ob-onConfigAcquire");
 }
 
-
-//config path
-let isGuestMode = false
-let MeetingId = "" //"21d666ba-a0d8-4db9-9643-e74525bafe1b"
-
-let fullDomain = "connector.eagle3dstreaming.com"
-
-
-//////////////////
-var owner = "demo" //"put your user name "
-let app = "E3DSFeaturesTemplate" //"put your app name"
-let configurationName = "E3DS-Iframe-Demo"//"put your config name"
-
-let search=window.location.search
-console.log("search: " + search)
-
-let urlParams = new URLSearchParams(search);
-console.log("urlParams: ")
-
-
-//?app=CarConfigurator427_reimport&owner=demo&configuration=E3DS-Iframe-Demo
-
-  for (var [key, value] of urlParams.entries()) 
-  {
-	  console.log(key, value);
-	var X= urlParams.get('app') 
-	var Y= urlParams.get('owner') 
-	var Z= urlParams.get('configuration') 
-	  
-	  if(X!= undefined)
-		  app=X
-	  
-	  
-	    if(Y!= undefined)
-		  owner=Y
-	  
-	    if(Z!= undefined)
-		  configurationName=Z
-	  
-  
-
-   
-  }
-  
-///////////////////////////////////////////////
-
-var part1 = "https://" + fullDomain
-var part2 = "/v5/" + owner + "/" + app + "/" + configurationName
-
-////////////////////////////
-
-
-/* 
-
-let search = ""
-
-if ((MeetingId == "") || (MeetingId == undefined)) {
-
-} else if ((MeetingId != "") || (MeetingId != undefined)) {
-    if (!isGuestMode)
-        search = "?hostMeetingId=" + MeetingId
-    else //type="text/css" rel="stylesheet" href="assets/css/voip.css"/>
-    {
-        part2 = "/v5/" + owner + "/" + configurationName
-
-        if ((MeetingId != "") || (MeetingId != undefined))
-            search = "?GuestMeetingId=" + MeetingId
-    }
-
+function onDataChannelOpen() {
+	console.log("ob-onDataChannelOpen");
+	var event = new CustomEvent("datachannel-event", { detail: "open" });
+	window.dispatchEvent(event);
 }
- */
 
+function onDataChannelClose() {
+	console.log("ob-onDataChannelClose");
+	var event = new CustomEvent("datachannel-event", { detail: "close" });
+	window.dispatchEvent(event);
+}
 
-
-
-
-
-
-
-let pathname = part2
-console.log("pathname: " + pathname)
-
-let href_emulated =part1 + part2
-
-
-href_emulated = href_emulated + search
-console.log("href_emulated1: " + href_emulated)
-console.log("pathname1: " + pathname)
-
+function onResponseFromUnreal(descriptor) {
+	console.log("ob-onResponseFromUnreal");
+	console.log("UnrealResponse: " + descriptor);
+	var event = new CustomEvent("game-event", { detail: descriptor });
+	window.dispatchEvent(event);
+}
--- src/App.js
+++ src/App.js
@@ -7,30 +7,38 @@ import E3DSReactComponent from './components/E3DSReactComponent';
 
 const App = () => {
 
-  window.addEventListener('game-event', (event) => {
-
-    console.log(event.detail);
-    setResponse(event.detail);
-
-  });
-
   const [pos, setPos] = useState("");
   const [response, setResponse] = useState("Sample text");
+  const [isDataChannelExists, setIsDataChannelExists] = useState(false);
+
+  window.addEventListener('game-event', (event) => {
+    console.log(event.detail);
+    setResponse(event.detail);
+  });
+
+  window.addEventListener('datachannel-event', (event) => {
+    console.log("ob-onDataChannelOpen event" + event.detail);
+    if (event.detail === "open") {
+      setIsDataChannelExists(true);
+    } else if (event.detail === "close") {
+      setIsDataChannelExists(false);
+    }
+  });
 
   const handleChange = e => {
     setPos(e.target.value);
   }
 
   const movePosition = () => {
-    window?.call2EmitUIInteraction({
+    window?.sendToUnreal({
       Teleport: pos
     });
   }
 
-  return <>
-    <Container className="p-3">
-      <h1 className="header">Welcome To E3DS Stream UI KIT Demo</h1>
-      <h2>
+
+  const ControlUi = () => {
+    if (isDataChannelExists) {
+      return <>
         <div>
           <button onClick={movePosition}>Move position to</button>
           <input
@@ -46,6 +54,20 @@ const App = () => {
             readOnly={true}
           />
         </div>
+
+      </>
+    } else {
+      return <>
+        <p>Loading...</p>
+      </>
+    }
+  }
+
+  return <>
+    <Container className="p-3">
+      <h1 className="header">Welcome To E3DS Stream UI KIT Demo</h1>
+      <h2>
+        <ControlUi />
         <E3DSReactComponent />
       </h2>
 
